f14dc06358ccbd47e28fac8d2cc48140ae5cf310
Mayank Chauhan <149558223+Mayankchauhan008@users.noreply.github.com>
2025-08-13 13:38:09 +0530

    Add files via upload

---
+++ simple_groq/Simple_groq_agent.py
@@ -0,0 +1,10 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = Agent(
+    model=Groq(id="llama-3.1-8b-instant")
+)
+agent.print_response("Write two sentence")
